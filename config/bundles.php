33d0290e985590ca3430d9f9fa1be64cf06dea20
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-05 17:43:07 +0200

    login system with token authentication

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
